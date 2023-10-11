cc72e9f57f0041faa1eeb5b1e3c42aa7a2fd6640
Itzik <itzik1058@gmail.com>
2023-10-11 18:23:04 +0300

    watcher loop

--- netwatch/__main__.py
+++ netwatch/__main__.py
@@ -10,13 +10,15 @@ from netwatch.core.database import get_session
 
 async def main():
     config: NetwatchConfig = instantiate(OmegaConf.load(NETWATCH_CONFIG_PATH))
-    for watcher in config.watchers:
-        with get_session() as session:
-            try:
-                await watcher.fetch(session)
-                session.commit()
-            except Exception:
-                pass
+    while True:
+        for watcher in config.watchers:
+            with get_session() as session:
+                try:
+                    await watcher.fetch(session)
+                    session.commit()
+                except Exception:
+                    pass
+        await asyncio.sleep(180)
 
 
 asyncio.get_event_loop().run_until_complete(main())
